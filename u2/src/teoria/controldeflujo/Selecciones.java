3ee349ec85c8a071dc7ed35fef031237ebea7602
Dario Marin Roth <laboratoriodecomputadoraselectronicas64@epet14.edu.ar>
2024-07-25 20:03:50 -0300

    Vemos el funcionamiento básico del if.

---
+++ u2/src/teoria/controldeflujo/Selecciones.java
@@ -0,0 +1,26 @@
+package teoria.controldeflujo;
+
+
+import java.util.Scanner;
+
+public class Selecciones {
+    public static void main(String[] args) {
+
+        Scanner ent = new Scanner(System.in);
+
+        System.out.println("Ingrese un valor para a: ");
+        int a = ent.nextInt();
+        System.out.println("Ingrese un valor para b: ");
+        int b = ent.nextInt();
+
+        System.out.println("Antes del if");
+
+        if(a != b) {
+            System.out.println("La expresión es verdadera");
+        }
+
+        System.out.println("Despues del if");
+    }
+}
+
+
