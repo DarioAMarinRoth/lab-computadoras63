94284bf79119a9ae752937cdd05baf478cf68046
Dario Marin Roth <laboratoriodecomputadoraselectronicas64@epet14.edu.ar>
2024-07-25 20:19:30 -0300

    Vemos la estructura while

---
+++ u2/src/teoria/controldeflujo/Iteraciones.java
@@ -0,0 +1,16 @@
+package teoria.controldeflujo;
+
+public class Iteraciones {
+    public static void main(String[] args) {
+
+        int contador = 0;
+        // mientras
+
+        while (contador < 5) {
+            contador++;
+            System.out.println("la expresión es verdadera");
+        }
+
+        System.out.println("terminó el loop.");
+    }
+}
